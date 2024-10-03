e07a8152ffe18759b16f4ebacb0f88457130db45
david <david.tai@akida.nl>
2024-10-03 15:50:02 +0200

    Update swagger

--- src/main/java/com/example/PokemonManagementSystem/config/SwaggerConfig.java
+++ src/main/java/com/example/PokemonManagementSystem/config/SwaggerConfig.java
@@ -1,17 +1,24 @@
 package com.example.PokemonManagementSystem.config;
 
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
     @Bean
-    public GroupedOpenApi api() {
-        return GroupedOpenApi.builder()
-                .group("public-api")
-                .pathsToMatch("/api/**") // Adjust to your API endpoints
-                .build();
+    public OpenAPI MusiQOpenAPI() {
+        OpenAPI openAPI = new OpenAPI();
+        openAPI.addServersItem(new Server().url("http://localhost:8080/api/").description("Localhost)"));
+        return openAPI
+                .info(new Info().title("Pokemon Management System")
+                        .description("Pokemon Management System")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("https://quintor.nl")));
     }
 }
+
+
