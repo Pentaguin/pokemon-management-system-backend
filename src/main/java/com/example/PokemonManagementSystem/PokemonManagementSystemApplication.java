362f0217277d03c9b484daea955f736b7c3cb1a9
david <david.tai@akida.nl>
2024-10-03 09:48:27 +0200

    Initial commit

---
+++ src/main/java/com/example/PokemonManagementSystem/PokemonManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.PokemonManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonManagementSystemApplication.class, args);
+	}
+
+}
